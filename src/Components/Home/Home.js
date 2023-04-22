c15f1128ffde7a4df8cab74f2521a5077e470ef0
Utkp5 <utkp200@gmail.com>
2023-04-22 22:11:21 +0530

    create display delete chng

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { FaPencilAlt } from "react-icons/fa";
 import { FaPlus } from "react-icons/fa";
 import { FaRegTrashAlt } from "react-icons/fa";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 
 function Home() {
 
@@ -20,8 +21,7 @@ function Home() {
                 if (data) {
                    console.log(data.data); 
                    console.log(`message created successfully`);
-                   settitle(" ");
-                   setdescription(" ");
+                  clearf()
                 }
             
         } catch (error) {
@@ -30,32 +30,86 @@ function Home() {
         }
     }
 
+    const clearf = () =>{
+      // settitle();
+      // setdescription('');
+      // window.location.reload();
+      diplaytodo();
+    }
+
+    
+//--------------------------------
+
+
+    //display message
+    const [display,setdisplay] = useState([])
+
+    const diplaytodo = async() => {
+
+      const res =  await axios.get("http://localhost:5000/api/displayTodo")
+      const final = res.data;
+      
+      if (final) {
+        setdisplay(final)
+        console.log(final);
+      }
+      
+    }
+
+    useEffect(() => {
+      diplaytodo();
+
+    },[])
+
+
+//-------------------------------------------------
+//delete message
+// const {id} = useParams();
+const deleteTodo = async(id) => {
+
+  const res =  await axios.delete(`http://localhost:5000/api/deleteTodo/${id}`)
+  clearf();
+  console.log(res.id);
+
+}
+
+//----------------------------------------------------
 
 
 
   return (
     <div>
+
       <div className="div_center">
         <h2>Pandit Utkarsh Myways Task</h2>
       </div>
+
 
       <div className="btn_center">
          <input type="text" name="title" id="input" placeholder="Title" onChange={(e) => settitle(e.target.value)}/>
          <input type="text" name="description" id="input" placeholder="Description" onChange={(e) => setdescription(e.target.value)}/>
          <button className="btn_add" onClick={createtodo}>Add&nbsp;<FaPlus size={19}/></button>
       </div>
-
-      <div className="div_flex">
       
-        <div className="card">
-            <div className="title">
-                <p>Title</p>
-                <p><FaPencilAlt size={19}/>&nbsp;&nbsp;&nbsp;&nbsp;<FaRegTrashAlt size={19}/></p>
-            </div>
-            <p className="des">description</p>
-        </div>
+      
+      <div className="div_flex">
+       {
+        display.map((dis) => {
 
+          return (
+            <div className="card" key={dis._id}>
+                <div className="title">
+                    <p>{dis.title}</p>
+                    <p><FaPencilAlt size={19}/>&nbsp;&nbsp;&nbsp;&nbsp;<FaRegTrashAlt size={19} onClick={() => deleteTodo(dis._id)}/></p>
+                </div>
+                <p className="des">{dis.description}</p>
+            </div>
+          )
+          })
+        }        
       </div>
+
+
 
     </div>
   );
