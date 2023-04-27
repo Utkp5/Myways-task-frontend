2ad5388ca39f10fe8cc94f7f59daac2a2e56d56c
Utkp5 <utkp200@gmail.com>
2023-04-27 20:49:47 +0530

    update  chng

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
+import Update from "./Components/Update/Update";
 
 function App() {
   return (
      <Router>
          <Routes>
              <Route exact path="/"  element={<Home />} />
+             <Route exact path="/Update-message"  element={<Update />} />
          </Routes>
      </Router>
   );
---
+++ src/Components/Update/Update.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Update.css';
+function Update() {
+  return (
+    <div>
+      hello update
+    </div>
+  )
+}
+
+export default Update
